fc900de122c582dda4647f2b9f5f9c65426fe163
WebCoder346 <sauravcoder1@gmail.com>
2024-08-11 19:02:02 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,59 +3,74 @@ const data = [
     "thumbnail": "img1.jpeg",
     "video": "video1.mp4",
     "title": "Iron Man 4 - Official Trailer _ Robert Downey Jr",
-    "info": "Marvel_Studio. 1.2M views 1M likes. "
+    "info": "Marvel_Studio. 1.2M views 1M likes. ",
+    "channel": "Marvel_Studio",
   },
   {
     "thumbnail": "img2.jpeg",
     "video": "video2.mp4",
     "title": "8 Years of Coding Journey in 8 Minutes🚀 How i started coding 💻 Software Engineer at Microsoft",
-    "info": "Nitin Chaudhary. 55k view. 30k."
-
+    "info": "Nitin Chaudhary. 55k view. 30k.",
+    "channel": "Nitin Chaudhary",
   },
   {
     "thumbnail": "img3.jpeg",
     "video": "video3.mp4",
     "title": "Complete road map of Web Development 💻 for Beginners",
-    "info": "@ezipres. 20k views 15k likes. "
+    "info": "@ezipres. 20k views 15k likes. ",
+    "channel": "@ezipres",
   },
   {
     "thumbnail": "img4.jpeg",
     "video": "video4.mp4",
     "title": "Iron Man 4 trailer.Robert Downey Jr Return official trailer form Marvel ",
-    "info": "Marvel_Studio. 1.5M views 1.1M likes. "
+    "info": "Marvel_Studio. 1.5M views 1.1M likes. ",
+    "channel": "Marvel_Studio",
   },
   {
     "thumbnail": "img5.jpeg",
     "video": "video5.mp4",
     "title": "This is How Hackers Crack Passwords! (Don't Try)",
-    "info": "Code With Harry. 1.5M views 1.2M likes. "
+    "info": "Code With Harry. 1.5M views 1.2M likes. ",
+    "channel": "Code Whith Harry",
   },
   {
     "thumbnail": "img6.jpeg",
     "video": "video6.mp4",
     "title": "RAM MANDIR | AYODHYA VLOG",
-    "info": "TOTAL GAMING. 1.5M views 1.2M likes. "
+    "info": "TOTAL GAMING. 1.5M views 1.2M likes. ",
+    "channel": "TOTAL GAMING",
   },
   {
     "thumbnail": "img7.jpeg",
     "video": "video7.mp4",
     "title": "Ajju Bhat vs India's number 1 MP40 player",
-    "info": "TOTAL GAMING. 3M views 2.8M likes. "
+    "info": "TOTAL GAMING. 3M views 2.8M likes. ",
+    "channel": "TOTAL GAMING",
     },
   {
     "thumbnail": "img8.jpeg",
     "video": "video8.mp4",
     "title": "Deewane Hum Nahi Hote (Slow and Reverb) | Lofi | Hindi - (Slow and Reverb) songs | Lyrical Audio",
-    "info": "Lofy player. 95k views 55k likes. "
+    "info": "Lofy player. 95k views 55k likes. ",
+    "channel": "Lofy player",
   },
   {
     "thumbnail": "img9.jpeg",
     "video": "video9.mp4",
     "title": "Iron Man Nuclear Missile Scene in Hindi The Avengers Hulk Saves Tony Stark Scene Movie Clip HD",
-    "info": "Marvel_Studio. 5M views 4.9M likes. "
+    "info": "Marvel_Studio. 5M views 4.9M likes. ",
+    'channel': "Marvel_Studio",
   },
   ]
+const fonts = {
+  "shorts": `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xml:space="preserve">
+    
+    <defs>
+    </defs>
+    <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(35.06303501945524 45.024124513618645) scale(1.83 1.83)" >
+    	<path d="M 69.186 38.498 c -1.406 -0.795 -2.751 -1.651 -4.34 -2.568 c 1.589 -0.978 3.057 -1.834 4.463 -2.629 c 6.541 -3.546 10.148 -10.759 9.047 -18.095 C 76.766 3.164 62.889 -3.804 52.375 2.186 C 41.616 8.361 30.857 14.413 20.22 20.77 c -8.436 5.074 -9.659 11.798 -8.314 19.745 c 0.856 4.952 4.157 8.619 8.497 11.126 c 1.528 0.856 2.995 1.712 4.646 2.69 c -1.834 1.1 -3.484 2.078 -5.257 2.995 c -7.825 4.952 -10.515 15.099 -6.113 23.291 c 4.646 8.62 15.405 11.921 24.024 7.275 c 9.292 -5.257 18.584 -10.637 27.815 -16.016 c 2.201 -1.284 4.524 -2.445 6.48 -4.035 C 81.84 59.649 80.373 44.795 69.186 38.498 z M 36.359 58.549 V 31.835 l 23.108 13.327 L 36.359 58.549 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+    </g>
+    </svg>`,
 
-// function getInd(ind) {
-//   printIndex(ind);
-// }
+}
