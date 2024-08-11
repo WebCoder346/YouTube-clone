4f7d838f348f6b81b58081bddc572089de835209
WebCoder346 <sauravcoder1@gmail.com>
2024-08-11 18:42:14 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,56 +1,56 @@
 const data = [
   {
-    "thumbnail": "/YouTube/images/thnlb/img1.jpeg",
-    "video": "/YouTube/video/video1.mp4",
+    "thumbnail": "img1.jpeg",
+    "video": "video1.mp4",
     "title": "Iron Man 4 - Official Trailer _ Robert Downey Jr",
     "info": "Marvel_Studio. 1.2M views 1M likes. "
   },
   {
-    "thumbnail": "/YouTube/images/thnlb/img2.jpeg",
-    "video": "/YouTube/video/video2.mp4",
+    "thumbnail": "img2.jpeg",
+    "video": "video2.mp4",
     "title": "8 Years of Coding Journey in 8 Minutes🚀 How i started coding 💻 Software Engineer at Microsoft",
     "info": "Nitin Chaudhary. 55k view. 30k."
 
   },
   {
-    "thumbnail": "/YouTube/images/thnlb/img3.jpeg",
-    "video": "/YouTube/video/video3.mp4",
+    "thumbnail": "img3.jpeg",
+    "video": "video3.mp4",
     "title": "Complete road map of Web Development 💻 for Beginners",
     "info": "@ezipres. 20k views 15k likes. "
   },
   {
-    "thumbnail": "/YouTube/images/thnlb/img4.jpeg",
-    "video": "/YouTube/video/video4.mp4",
+    "thumbnail": "img4.jpeg",
+    "video": "video4.mp4",
     "title": "Iron Man 4 trailer.Robert Downey Jr Return official trailer form Marvel ",
     "info": "Marvel_Studio. 1.5M views 1.1M likes. "
   },
   {
-    "thumbnail": "/YouTube/images/thnlb/img5.jpeg",
-    "video": "/YouTube/video/video5.mp4",
+    "thumbnail": "img5.jpeg",
+    "video": "video5.mp4",
     "title": "This is How Hackers Crack Passwords! (Don't Try)",
     "info": "Code With Harry. 1.5M views 1.2M likes. "
   },
   {
-    "thumbnail": "/YouTube/images/thnlb/img6.jpeg",
-    "video": "/YouTube/video/video6.mp4",
+    "thumbnail": "img6.jpeg",
+    "video": "video6.mp4",
     "title": "RAM MANDIR | AYODHYA VLOG",
     "info": "TOTAL GAMING. 1.5M views 1.2M likes. "
   },
   {
-    "thumbnail": "/YouTube/images/thnlb/img7.jpeg",
-    "video": "/YouTube/video/video7.mp4",
+    "thumbnail": "img7.jpeg",
+    "video": "video7.mp4",
     "title": "Ajju Bhat vs India's number 1 MP40 player",
     "info": "TOTAL GAMING. 3M views 2.8M likes. "
     },
   {
-    "thumbnail": "/YouTube/images/thnlb/img8.jpeg",
-    "video": "/YouTube/video/video8.mp4",
+    "thumbnail": "img8.jpeg",
+    "video": "video8.mp4",
     "title": "Deewane Hum Nahi Hote (Slow and Reverb) | Lofi | Hindi - (Slow and Reverb) songs | Lyrical Audio",
     "info": "Lofy player. 95k views 55k likes. "
   },
   {
-    "thumbnail": "/YouTube/images/thnlb/img9.jpeg",
-    "video": "/YouTube/video/video9.mp4",
+    "thumbnail": "img9.jpeg",
+    "video": "video9.mp4",
     "title": "Iron Man Nuclear Missile Scene in Hindi The Avengers Hulk Saves Tony Stark Scene Movie Clip HD",
     "info": "Marvel_Studio. 5M views 4.9M likes. "
   },
